66c66bcdf737911c131b72330005d3c188bc6213
GabeJason <gablications@gmail.com>
2016-12-05 15:16:07 -0500

    Added Models Enhanced User Interaction in VR mode

--- app/src/main/java/com/apshop/actionplus/apshop/VrModeActivity.java
+++ app/src/main/java/com/apshop/actionplus/apshop/VrModeActivity.java
@@ -11,6 +11,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 /**
@@ -21,6 +23,8 @@ public class VrModeActivity extends Activity {
 
     WebView myWebView;
     int productNum;
+    Button contBtn;
+    RelativeLayout introLay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +34,11 @@ public class VrModeActivity extends Activity {
 
         myWebView = (WebView) findViewById(R.id.vrWebView);
         WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
         productNum = Integer.parseInt(getIntent().getExtras().getString("productNum"));
+
+        contBtn = (Button)findViewById(R.id.vrmodeContBtn);
+        introLay = (RelativeLayout)findViewById(R.id.vrmodeIntroLay);
 
         myWebView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,11 +53,21 @@ public class VrModeActivity extends Activity {
 
         if(isConnected) {
             switch (productNum) {
+                case 2:
+                    //Impact
+                    myWebView.loadUrl("https://sketchfab.com/models/f0a4132d2f3446bf8c365c7cc270ffaf/embed?cardboard=1");
+                    break;
                 case 3:
+                    //Everest
                     myWebView.loadUrl("https://sketchfab.com/models/00c8a4f6ba3244ca82a73bf37f8063a7/embed?cardboard=1");
                     break;
                 case 4:
+                    //Continental
                     myWebView.loadUrl("https://sketchfab.com/models/8a9f5f7f2cfa420281d91458c8046770/embed?cardboard=1");
+                    break;
+                case 5:
+                    //Airtext Pen
+                    myWebView.loadUrl("https://sketchfab.com/models/2035063665ed430f849c9cdc6df98b2d/embed?cardboard=1");
                     break;
                 default:
                     myWebView.loadUrl("https://sketchfab.com/models/00c8a4f6ba3244ca82a73bf37f8063a7/embed?cardboard=1");
@@ -58,6 +76,14 @@ public class VrModeActivity extends Activity {
         }else{
             Toast.makeText(getApplicationContext(), "Connect to Data or WIFI network and try again.", Toast.LENGTH_LONG).show();
         }
+
+        contBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myWebView.setVisibility(View.VISIBLE);
+                introLay.setVisibility(View.GONE);
+            }
+        });
     }
 
     @Override
