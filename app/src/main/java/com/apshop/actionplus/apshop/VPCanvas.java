c5389f92eee35209298fee1716eec68ee1c30a98
GabeJason <gablications@gmail.com>
2016-12-16 15:42:02 -0500

    Virtual Proof in Development More

--- app/src/main/java/com/apshop/actionplus/apshop/VPCanvas.java
+++ app/src/main/java/com/apshop/actionplus/apshop/VPCanvas.java
@@ -7,7 +7,9 @@ import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 /**
  * Created by Gabriel Jason on 12/15/2016.
@@ -15,10 +17,18 @@ import android.view.View;
 
 public class VPCanvas extends View {
 
+    int width;
+    int height;
+    float viewWidth;
+    float viewHeight;
+    Bitmap.Config config = Bitmap.Config.ARGB_8888;
 
+    RelativeLayout mainLay;
 
     public VPCanvas(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        mainLay = (RelativeLayout) findViewById(R.id.activity_virtual_proof);
 
         BitmapFactory.Options opts = new BitmapFactory.Options();
         opts.inMutable = true;
@@ -41,11 +51,26 @@ public class VPCanvas extends View {
             }
 
         }*/
+        Log.i ("BM Width", Integer.toString(bm.getWidth()));
         Log.i("Mutable", Boolean.toString(bm.isMutable()));
         //bm.setPixels(pixels1, 0, bm.getWidth(), 0, 0, bm.getWidth(), bm.getHeight());
         Bitmap b2 = BitmapFactory.decodeResource(getResources(), R.drawable.actionpluslogo25, opts);
         int[] pixels2 = new int[b2.getWidth() * b2.getHeight()];
         b2.getPixels(pixels2, 0, b2.getWidth(), 0, 0, b2.getWidth(), b2.getHeight());
+        float pixlen = pixels2.length;
+        int scale2 = (int)Math.round(pixlen * .25);
+        int[] pixels2Scale = new int[scale2];
+        Log.i("pix2scale",Integer.toString((int)Math.round(pixlen * .25)));
+        int countdown = 0;
+        for (int i = 0; i < pixels2.length; i++){
+            if (i % 4 == 3){
+                pixels2Scale[i + countdown] = pixels2[i];
+            }else{
+                countdown = countdown -1;
+            }
+        }
+        Log.i ("Testing","Testing");
+        pixels2 = pixels2Scale;
         boolean changed = false;
         for (int i = 0; i < pixels2.length; i++){
             if (pixels2[i] == Color.WHITE) {
@@ -70,21 +95,26 @@ public class VPCanvas extends View {
             changed = false;
 
         }
-        b2.setPixels(pixels2, 0, b2.getWidth(), 0, 0, b2.getWidth(), b2.getHeight());
+        int b2width = (int) Math.round(b2.getWidth() * .25);
+        int b2height = (int) Math.round(b2.getHeight() * .25);
+        Bitmap b2Temp = Bitmap.createBitmap(pixels2, b2width, b2height, config);
+        b2 = b2Temp;
+        //b2.setPixels(pixels2, 0, b2.getWidth(), 0, 0, b2.getWidth(), b2.getHeight());
 
-        int width;
-        int height;
+
         if(bm.getWidth() >= b2.getWidth()){
             width = bm.getWidth();
         }else{
             width = b2.getWidth();
         }
+
+        Log.i("Comb Width", Integer.toString(width));
         if(bm.getHeight() >= b2.getHeight()){
             height = bm.getHeight();
         }else{
             height = b2.getHeight();
         }
-
+        Log.i("Comb Height", Integer.toString(height));
         int[] pixels = new int[width * height];
 
     /*    for (int i = 0; i < pixels.length; i++){
@@ -116,24 +146,38 @@ public class VPCanvas extends View {
                 heightR = true;
             }
         }*/
+        int count = 0;
         Log.i ("Spacing", Integer.toString(spacing));
         for (int i = 0; i < pixels.length; i++){
             if(n < pixels1.length) {
                 pixels[i] = pixels1[n];
+                count = count + 1;
                 n++;
-                if ((i / bm.getWidth()) == 1 && spacing > 0) {
-                    q = i +spacing;
+                if ( i > 1) {
+                    if (i == bm.getWidth()){
+                        q = i + spacing;
+                        count = 0;
+                        while (i < q) {
+                            pixels[i] = Color.YELLOW;
+                            i = i + 1;
+                        }
+                    }else if (i > bm.getWidth() && count > (bm.getWidth() - 1) && spacing > 0) {
+                        count = 0;
+                        Log.i("I", Integer.toString(i));
+                        q = i + spacing;
+                        Log.i("Spacing", Integer.toString(q));
                     /*for (i = i + 1; i < q; i++) {
                         pixels[i] = Color.WHITE;
                     }*/
-                    i = i + 1;
-                    while (i < q){
-                        pixels[i] = Color.BLUE;
                         i = i + 1;
+                        while (i < q) {
+                            pixels[i] = Color.YELLOW;
+                            i = i + 1;
+                        }
                     }
                 }
             }else{
-                pixels[i] = Color.WHITE;
+                pixels[i] = Color.YELLOW;
             }
 
         }
@@ -143,21 +187,137 @@ public class VPCanvas extends View {
             spacing = (bm.getWidth() - b2.getWidth());
         }
         n = 0;
+        count = 0;
         for (int i = 0; i < pixels2.length; i++){
-            if (n < pixels2.length) {
+            if (i < pixels2.length) {
                 if (pixels2[i] == Color.BLUE) {
                     pixels[n] = Color.BLUE;
                 }
-                if ((i / b2.getWidth()) == 1) {
+                if (count > (b2.getWidth() -1)) {
                     n = n + spacing;
+                    count = 0;
                 }
                 n++;
+                count++;
             }
         }
 
-        Bitmap.Config config = Bitmap.Config.ARGB_8888;
-        Bitmap combBit = Bitmap.createBitmap(pixels, width, height, config);
+
+
+        Bitmap BScale = Bitmap.createBitmap(pixels, width, height, config);
+
+
+
+        int returncalc = calcRatio();
+        int[] pixelsScaled = new int[width * height];
+
+        if (returncalc == 1) {
+            for (int i = 0; i < pixelsScaled.length; i++) {
+                if (i < pixels.length) {
+                    pixelsScaled[i] = pixels[i];
+                } else {
+                    pixelsScaled[i] = Color.GREEN;
+                }
+            }
+        }else if(returncalc > 1){
+            n = 0;
+            q =0;
+            count = 0;
+            spacing = width - returncalc;
+            Log.i ("Spacing", Integer.toString(spacing));
+            for (int i = 0; i < pixelsScaled.length; i++){
+                if(n < pixels.length) {
+                    pixelsScaled[i] = pixels[n];
+                    count = count + 1;
+                    n++;
+                    if ( i > 1) {
+                        if (i == returncalc){
+                            q = i + spacing;
+                            count = 0;
+                            while (i < q) {
+                                pixels[i] = Color.YELLOW;
+                                i = i + 1;
+                            }
+                        }else if (i > returncalc && count > (returncalc - 1)) {
+                            count = 0;
+                            Log.i("I", Integer.toString(i));
+                            q = i + spacing;
+                            Log.i("Spacing", Integer.toString(q));
+                            i = i + 1;
+                            while (i < q && i < pixelsScaled.length) {
+                                pixels[i] = Color.YELLOW;
+                                i = i + 1;
+                            }
+                        }
+                    }
+                }else if (i < pixelsScaled.length){
+                    pixels[i] = Color.YELLOW;
+                }
+
+            }
+        }
+        Log.i ("Width before", Integer.toString(width));
+        Bitmap combBit = Bitmap.createBitmap(pixelsScaled, width, height, config);
         BitmapDrawable bd = new BitmapDrawable(getContext().getResources(), combBit);
+
         getRootView().setBackground(bd);
     }
+
+    public int calcRatio(){
+        viewWidth = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 400, getResources().getDisplayMetrics());
+        viewHeight = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 300, getResources().getDisplayMetrics());
+
+        Log.i ("View Width", Float.toString(viewWidth));
+        Log.i ("View Height", Float.toString(viewHeight));
+        boolean testheight = false;
+        boolean testwidth = false;
+        boolean equal = false;
+
+
+        float fwidth = width;
+        float fheight = height;
+
+        int oldWidth = 0;
+
+
+        float ratioViewWH = viewHeight / viewWidth; // 900/500 = 1.8 to multiply width to get proper height
+        float ratioViewHW = viewWidth / viewHeight; // 500/900 = .55555 to multiply height to get proper width
+
+        float ratioWH = fheight / fwidth; // 900/500 = 1.8 to multiply width to get proper height
+        float ratioHW = fwidth / fheight; // 500/900 = .55555 to multiply height to get proper width
+
+        Log.i ("RatioView W H", Float.toString(ratioViewWH));
+        Log.i ("RatioView H W", Float.toString(ratioViewHW));
+
+        Log.i ("Ratio W H", Float.toString(ratioWH));
+        Log.i ("Ratio H W", Float.toString(ratioHW));
+
+        if (ratioViewWH > ratioWH) {
+            testwidth = true;
+        }
+
+        if (ratioViewHW > ratioHW){
+            testheight = true;
+        }
+
+        Log.i ("TestWidth", Boolean.toString(testwidth));
+        Log.i ("TestHeight", Boolean.toString(testheight));
+
+        Log.i ("Height", Integer.toString(height));
+        if (testwidth){
+            height = Math.round(fwidth * ratioViewWH);
+            Log.i ("Height", Integer.toString(height));
+            return 1;
+        }
+
+        if (testheight){
+            oldWidth = width;
+            width = Math.round(fheight * ratioViewHW);
+            return oldWidth;
+        }
+        return 0;
+
+    }
+
+
 }
